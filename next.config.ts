0bd5317fee345ef276b285249f2dc81aa1f79538
Miguel Tokumoto <m.tokumoto@gmail.com>
2025-01-29 06:50:01 -0500

    Refactor Next.js ESLint configuration with cleaner implementation

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Option 1: Disable ESLint completely during builds
+    ignoreDuringBuilds: true,
+
+    // Option 2: Configure specific rules
+    // This is commented out since we're using Option 1 above
+    /*
+    dirs: ['pages', 'components', 'lib', 'src', 'app'],  // Directories to lint
+    ignoreDuringBuilds: false,  // Don't ignore during builds
+    */
+  },
 };
 
 export default nextConfig;
-
-eslint: {
-  ignoreDuringBuilds: true,
-}
